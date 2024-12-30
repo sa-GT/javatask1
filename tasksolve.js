1d7fe0d291774418c89aae37ba435e17674bfc11
sa-GT <habeeb.alafeef6@gmail.com>
2024-12-30 17:19:32 +0300

    dd

---
+++ tasksolve.js
@@ -0,0 +1,98 @@
+// zakat
+var x = (250 * 2.5)/100
+console.log(x)
+// array 1 --> 
+let r =[ 1,7,9,45]
+let s =["Str","alex","moh",'the','fox','over',"lazy", 'dog']
+// array 2
+// var fruits=["Tomato","Banana","Watermelon"]
+// console.log(fruits.indexOf("Tomato"));
+// console.log(fruits.indexOf("Banana"));
+//Create array
+let Favorite_food=["malokhia","mgdara","qabsa"]
+let Favorite_Sport=["swimming","footbal","Nan","ride horse"]
+let Favorite_Movie=["shooter","loard of ring","ip Man","war"]
+//First value in array
+// var array=[1,4,5]
+// var vv =array[2]
+// console.log(vv);
+// var ss= ["t","u","g","x"]
+//var st = array[3]
+// console.log(st);
+// 6 var with
+// var array = [0,5,7,9]
+// array.shift()
+// array.shift()
+// array.shift()
+// array.push(10)
+// array.unshift(8)
+// array.unshift(6)
+// array.unshift(4)
+// array.unshift(3)
+// array.unshift(1)
+// console.log(array);
+//---------------------------------------------------------------------
+//7 Try and figure out
+// var array = [5,9,-7,3.5] 
+// array.shift()
+// array.unshift(10)
+// array.unshift(12)
+// array.pop()
+// array.push(70)
+// console.log(array)
+// --------------------------------------------------------------------
+//8 Question :
+// var arr1 = [ -3, 8, 7, 6, 5, -4, 3, 2, 1 ]
+// arr1.sort()
+// console.log(arr1)
+//--------------------------------------------------------------------
+//9 Question 
+// function testGpa(num){
+//     switch(true){
+//         case (50 <= num && num<= 59):
+//             console.log("E")
+//             break
+//         case (60 <= num && num<= 69):
+//             console.log("D")
+//             break;
+//         case (70 <= num && num<= 79):
+//             console.log("C")
+//             break;
+//         case (80 <= num && num<= 89):
+//             console.log("B")
+//             break;
+//         case (90 <= num && num<= 100) :
+//             console.log("A")
+//             break;
+//         default:
+//             console.log("None")
+//     }
+// }
+// testGpa(90)
+//-----------------------------------------------------------------------
+//Question 10 :
+// var arrs=[1]
+// console.log(arrs.toString())
+// arrs.push(2)
+// console.log(arrs.toString())
+// arrs.push(3)
+// console.log(arrs.toString())
+// arrs.push(4)
+// console.log(arrs.toString())
+// arrs.push(5)
+// console.log(arrs.toString())
+// arrs.push(6)
+// console.log(arrs.toString())
+// arrs.push(7)
+// console.log(arrs.toString())
+// arrs.push(8)
+// console.log(arrs.toString())
+// arrs.push(9)
+// console.log(arrs.toString())
+// arrs.push(10)
+
+
+
+
+
+
